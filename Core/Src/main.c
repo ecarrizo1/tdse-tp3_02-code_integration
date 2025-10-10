0732cf43ee80de684aac98c0bbda6148b9716770
ecarrizo1 <ecarrizo@fi.uba.ar>
2025-10-10 12:36:47 -0300

    work in progress

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -203,8 +203,8 @@ static void MX_USART2_UART_Init(void)
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
@@ -213,7 +213,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LD2_Pin|LCD_D7_Pin|LCD_E_Pin|LCD_RS_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, LCD_D6_Pin|LCD_D5_Pin|LCD_D4_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
@@ -221,19 +224,26 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : LD2_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin;
+  /*Configure GPIO pins : LD2_Pin LCD_D7_Pin LCD_E_Pin LCD_RS_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin|LCD_D7_Pin|LCD_E_Pin|LCD_RS_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LCD_D6_Pin LCD_D5_Pin LCD_D4_Pin */
+  GPIO_InitStruct.Pin = LCD_D6_Pin|LCD_D5_Pin|LCD_D4_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
   HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
@@ -254,8 +264,7 @@ void Error_Handler(void)
   }
   /* USER CODE END Error_Handler_Debug */
 }
-
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
   * @brief  Reports the name of the source file and the source line number
   *         where the assert_param error has occurred.
--- app/inc/board.h
+++ app/inc/board.h
@@ -57,12 +57,47 @@ extern "C" {
 #define STM32F407G_DISC1	(8)
 #define STM32F429I_DISC1	(9)
 
-#define BOARD (NUCLEO_F103RC)
+#define BOARD (NUCLEO_F103RB)
 
 /* STM32 Nucleo Boards - 32 Pins */
 #if (BOARD == NUCLEO_F303R8)
 
 #endif
+
+/* STM32 Nucleo Boards - 64 Pins */
+#if ((BOARD == NUCLEO_F103RB))
+
+#define BTN_A_PIN		B1_Pin
+#define BTN_A_PORT		B1_GPIO_Port
+#define BTN_A_PRESSED	GPIO_PIN_RESET
+#define BTN_A_HOVER		GPIO_PIN_SET
+
+#define LED_A_PIN		LD2_Pin
+#define LED_A_PORT		LD2_GPIO_Port
+#define LED_A_ON		GPIO_PIN_SET
+#define LED_A_OFF		GPIO_PIN_RESET
+
+// Probando pines y puertos
+#define LCD_D4_PIN     LCD_D4_Pin
+#define LCD_D4_PORT    GPIOB
+
+#define LCD_D5_PIN     LCD_D5_Pin
+#define LCD_D5_PORT    GPIOB
+
+#define LCD_D6_PIN     LCD_D6_Pin
+#define LCD_D6_PORT    GPIOB
+
+#define LCD_D7_PIN     LCD_D7_Pin
+#define LCD_D7_PORT    GPIOA
+
+#define LCD_RS_PIN     LCD_RS_Pin
+#define LCD_RS_PORT    GPIOA
+
+#define LCD_EN_PIN     LCD_E_Pin
+#define LCD_EN_PORT    GPIOA
+
+#endif
+
 
 /* STM32 Nucleo Boards - 64 Pins */
 #if ((BOARD == NUCLEO_F103RC) || (BOARD == NUCLEO_F401RE) || (BOARD == NUCLEO_F446RE))
